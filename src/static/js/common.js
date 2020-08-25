65214719f878ffd42ef78198e5a700eea32d99c9
Aslan <berezg86@gmail.com>
2020-08-25 22:30:39 +0300

    Мобильная шапка

--- src/static/js/common.js
+++ src/static/js/common.js
@@ -2,4 +2,14 @@ $(document).ready(function () {
     $('.search-desctop').hover(function () {
         $(this).toggleClass('is-active');
     });
+
+    $('.catalog-show').click(function () {
+        $('.catalog-list').toggleClass('is-active');
+    });
+
+    $('.burger').click(function () {
+        $(this).toggleClass('is-active');
+        $('.dropdownmenu').toggleClass('is-active');
+    });
+
 });
