615582738b19bcc14e8f648b0689a71d012670b8
Aslan <berezg86@gmail.com>
2020-08-27 15:07:14 +0300

    слайдеры на главной

--- src/static/js/common.js
+++ src/static/js/common.js
@@ -14,7 +14,7 @@ $(document).ready(function () {
 
     var bannerSlider = new Swiper('.banner-slider', {
         slidesPerView: 1,
-        spaceBetween: 0,
+        spaceBetween: 32,
         loop: true,
         navigation: {
             nextEl: '.swiper-button-next',
@@ -24,6 +24,110 @@ $(document).ready(function () {
             el: '.swiper-pagination',
             clickable: true,
         },
+    });
+
+    var contSlider = new Swiper('.sale-slider', {
+        slidesPerView: 5,
+        spaceBetween: 0,
+        loop: true,
+        navigation: {
+            nextEl: '.swiper-button-next-sale',
+            prevEl: '.swiper-button-prev-sale',
+        },
+        breakpoints: {
+            499: {
+                slidesPerView: 1,
+                spaceBetween: 20,
+            },
+            767: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+            },
+            991: {
+                slidesPerView: 3,
+                spaceBetween: 20,
+            },
+            1279: {
+                slidesPerView: 4,
+                spaceBetween: 20,
+            }
+        }
+    });
+
+    var newsSlider = new Swiper('.news-slider', {
+        slidesPerView: 5,
+        spaceBetween: 0,
+        loop: true,
+        navigation: {
+            nextEl: '.swiper-button-next-news',
+            prevEl: '.swiper-button-prev-news',
+        },
+        breakpoints: {
+            499: {
+                slidesPerView: 1,
+                spaceBetween: 20,
+            },
+            767: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+            },
+            991: {
+                slidesPerView: 3,
+                spaceBetween: 20,
+            },
+            1279: {
+                slidesPerView: 4,
+                spaceBetween: 20,
+            }
+        }
+    });
+
+    var leadSlider = new Swiper('.lead-slider', {
+        slidesPerView: 5,
+        spaceBetween: 0,
+        loop: true,
+        navigation: {
+            nextEl: '.swiper-button-next-lead',
+            prevEl: '.swiper-button-prev-lead',
+        },
+        breakpoints: {
+            499: {
+                slidesPerView: 1,
+                spaceBetween: 20,
+            },
+            767: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+            },
+            991: {
+                slidesPerView: 3,
+                spaceBetween: 20,
+            },
+            1279: {
+                slidesPerView: 4,
+                spaceBetween: 20,
+            }
+        }
+    });
+
+    var complectSliderL = new Swiper('.csl', {
+        slidesPerView: 1,
+        spaceBetween: 0,
+        loop: true,
+        navigation: {
+            nextEl: '.swiper-button-next-cl',
+            prevEl: '.swiper-button-prev-cl',
+        }
+    });
+
+    var complectSliderR = new Swiper('.csr', {
+        slidesPerView: 1,
+        spaceBetween: 0,
+        loop: true,
+        navigation: {
+            nextEl: '.swiper-button-next-cr',
+            prevEl: '.swiper-button-prev-cr',
+        }
     });
 
 });
